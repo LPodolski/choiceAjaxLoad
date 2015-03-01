e65823e6fae9232ffb30c136bcc027d49d4decba
LPodolski <lpodolski@lpodolski.com>
2015-03-01 20:18:53 +0100

    basic entity / form setup

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,16 +2,32 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Item;
+use AppBundle\Form\ItemType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('default/index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $item = new Item();
+        $form = $this->createForm(new ItemType(), $item);
+
+        if ($form->handleRequest($request)->isValid()) {
+            $em->persist($item);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('homepage'));
+        }
+
+        return $this->render('default/index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
---
+++ src/AppBundle/Entity/Item.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Item
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Item
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="choiceField", type="string", length=255)
+     */
+    private $choiceField;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set choiceField
+     *
+     * @param string $choiceField
+     * @return Item
+     */
+    public function setChoiceField($choiceField)
+    {
+        $this->choiceField = $choiceField;
+
+        return $this;
+    }
+
+    /**
+     * Get choiceField
+     *
+     * @return string 
+     */
+    public function getChoiceField()
+    {
+        return $this->choiceField;
+    }
+}
